8bd346e3e5c8f9cd8e515793c9d505987518ea6b
uuzou <uuzou@qq.cp>
2018-11-07 11:25:57 +0800

    file

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+git co  zxm1
+echo "git co  zxm1"
+git pull origin zxm1
+echo "pull origin zxm1"
+git co master
+echo "git co master"
+git  merge zxm1
+echo "git  merge zxm1"
+
+:<<!
+git co  zxm2
+git pull origin zxm2
+git co master
+git  merge zxm2
+
+
+git co  zxm3
+git pull origin zxm3
+git co master
+git  merge zxm3
+
+git push origin master
+!
